8cdc8ca1c4b74942631b5d906fd99638630f1a0a
hchiam <hchiam@users.noreply.github.com>
2019-05-20 16:10:56 -0400

    console log examples

--- random/node/node-demo.js
+++ random/node/node-demo.js
@@ -1,1 +1,8 @@
-console.log('hi');
+let fake = require('faker');
+console.log(
+`_________________________
+WELCOME TO THE FAKE SHOP:
+-------------------------`);
+for (let i=0; i<10; i++) {
+    console.log(`${i+1}) ${fake.commerce.productName()} - $${fake.commerce.price()}`);
+}
