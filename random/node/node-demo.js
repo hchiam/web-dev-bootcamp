ff9814e8443b7882ce88cd18a95f0a6a06ea7f1d
hchiam <hchiam@users.noreply.github.com>
2019-05-20 16:25:09 -0400

    add test fake user info

--- random/node/node-demo.js
+++ random/node/node-demo.js
@@ -1,8 +1,14 @@
 let fake = require('faker');
+
 console.log(
 `_________________________
 WELCOME TO THE FAKE SHOP:
 -------------------------`);
+
 for (let i=0; i<10; i++) {
     console.log(`${i+1}) ${fake.commerce.productName()} - $${fake.commerce.price()}`);
 }
+
+console.log('-------------------------');
+console.log('Contact info:');
+console.log(fake.helpers.userCard());
