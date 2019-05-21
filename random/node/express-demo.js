74d5c05fdfe4d1fef4c28a4ba88d5c057aa3c761
hchiam <hchiam@users.noreply.github.com>
2019-05-20 22:43:22 -0400

    add url vars example

--- random/node/express-demo.js
+++ random/node/express-demo.js
@@ -17,6 +17,19 @@ app.get('/bye', (req, res) => {
     res.send('Goodbye!');
 });
 
+// '/r/:subredditName' covers only '/r/.../' but NOT just anything like '/r/.../.../.../etc...'
+app.get('/r/:subredditName', (req, res) => {
+    let subreddit = req.params.subredditName.toUpperCase();
+    res.send(`Welcome to the ${subreddit} subreddit!`);
+});
+
+app.get('/r/:subredditName/comments/:id/:title', (req, res) => {
+    console.log(req.params.subredditName);
+    console.log(req.params.id);
+    console.log(req.params.title);
+    res.send('comments page');
+});
+
 // any other URL beside the ones above
 app.get('*', (req, res) => {
     res.send('Page could not be found.');
